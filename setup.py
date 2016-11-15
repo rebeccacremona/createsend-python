7cd8be21f89fa7bb61dafb31da9ad4a64058bf5b
Rebecca Lynn Cremona <rebeccacremona@users.noreply.github.com>
2016-11-15 17:07:24 -0500

    Setup.py cannot depend on six.

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ import sys
 import os
 from distutils.core import setup
 
-from createsend.createsend import __version__
-
 setup(name = "createsend",
-      version = __version__,
+      version = '1.0.1',
       description = "A library which implements the complete functionality of the Campaign Monitor API.",
       author = "James Dennes",
       author_email = 'jdennes@gmail.com',
